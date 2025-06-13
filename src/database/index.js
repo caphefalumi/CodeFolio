815d33547a657d4e18555ef5726305fcc3956e43
caphefalumi <dangduytoan13l@gmail.com>
2025-06-13 15:26:51 +0700

    Test db

---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/admin');
+
+    console.log("OK");
+    console.log('MongoDB Connected...');
+
+  } catch (err) {
+    console.error('Database connection error:', err);
+    process.exit(1);
+  }
+};
+connectDB();
+export default connectDB;
