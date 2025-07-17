7f421d80f46478234fb66f47f2599187ae5cf55d
caphefalumi <dangduytoan13l@gmail.com>
2025-07-17 22:31:49 +0700

    Project Done, Final Commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import { fileURLToPath, URL } from "node:url"
 import "dotenv/config"
-const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
 	resolve: {
